365baa3bed0defded3c2837f638b9aee665097ad
eden <145543581+cardboardgh0st@users.noreply.github.com>
2023-11-01 11:06:28 +0100

    update

--- to do list/todolist.js
+++ to do list/todolist.js
@@ -12,10 +12,22 @@ function ajouterElement(){
 monChampTexte.value = ""
 }
 
+//faire un bouton tout effacer
+let boutonDeux = document.querySelector("#bouton_effacer")
+boutonDeux.addEventListener("click", toutEffacer)
+
+function toutEffacer(){
+//queryselectorall-->tableau, queryselector-->objet. tous_les_li est un tableau
+    let tous_les_li = document.querySelectorAll("li");
+//comme tous_les_li est un tableau (plusieurs éléments), il faut faire des boucles
+    for (let compteur=0 /*initialisation*/;compteur<tous_les_li.length /*combien de fois l'action est faite*/; compteur++ /*on passe du stylo 0 au stylo 1 au stylo 2. il faut préciser le pattern. ++=+1 */){
+        tous_les_li[compteur].remove();
+    }
+}
+
 
 //passe la souris sur h1-->son s'enclenche
 let monTitre=document.querySelector("h1")
-
 /* 
     monTitre.addEventListener("mouseover",jouerSon)
 function jouerSon({
@@ -23,3 +35,16 @@ function jouerSon({
     bruit.play();
 })
 */
+//marche pas
+let boutonTrois = document.querySelector("#bouton_de")
+boutonDeux.addEventListener("click", lancerDes)
+
+
+function lancerDes(){
+    let nombreAlea = math.floor(math.random()*6)+1;
+    console.log(nombreAlea)
+
+    let de=document.createElement("div");
+    de.innerText=nombreAlea
+    document.body.appendChild(de)
+}
